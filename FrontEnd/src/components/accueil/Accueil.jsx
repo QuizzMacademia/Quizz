a6a018d48fc7c4cd76a12b9e9c64fe29d8270a30
Abdelkafi Amal <zaghdanamal@yahoo.fr>
2020-05-19 17:45:23 +0200

    creation de la page Home

--- FrontEnd/src/components/accueil/Accueil.jsx
+++ FrontEnd/src/components/accueil/Accueil.jsx
@@ -5,17 +5,14 @@ import Training from "./Training";
 import Menu from "./menu";
 import {Switch} from "react-router";
 import Certificat from "./certificat";
+import Home from "./home";
 
 const Accueil = () => {
     return (
         <div>
             <Menu/>
             <Switch>
-                <Route exact path='/Accueil' render={() => <Fragment>
-                                                                <div className="question">
-                                                                    <h4>Bienvenue </h4>
-                                                                </div>
-                                                            </Fragment>}/>
+                <Route exact path='/Accueil' component ={Home}/>
                 <Route exact path='/Accueil/Entainement' component={Training}/>
                 <Route exact path='/Accueil/QCM' render={() => <h1> Q C M</h1>}/>
                 <Route exact path='/Accueil/Certificat' component={Certificat}/>
