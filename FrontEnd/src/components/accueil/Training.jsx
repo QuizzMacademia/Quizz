a4e479581e45bbb7e242c01636003463a13c3ee6
Stephane PASTORE <stephane.pastore@isil.fr>
2020-06-03 16:18:06 +0200

    Commexion backend component Question

--- FrontEnd/src/components/accueil/Training.jsx
+++ FrontEnd/src/components/accueil/Training.jsx
@@ -1,21 +1,25 @@
-import React, {Fragment, useState} from 'react';
-import MOCK_QUESTIONNAIRE from "../shared/mock/mock_questionnaire";
+import React, {Fragment, useEffect, useState} from 'react';
 import * as Yup from "yup";
 import {Formik} from "formik";
 import './question.css';
 import Question from "./question";
+import axios from "axios";
 
 function Training() {
 
     const [index, setIndex] = useState(0);
+    const [quizzSize] = useState(10);
     const [showAnswer, setShowAnswer] = useState(false);
-    const questionData = MOCK_QUESTIONNAIRE.questions[index];
+//    const questionData = MOCK_QUESTIONNAIRE.questions[index];
+    const [questionData, setQuestionData] = useState({});
     const [lastQuestion, setLastQuestion] = useState(false);
     const [listQuestion, setListQuestion] = useState(true);
     const [showButton, setShowButton] = useState(false);
     const [answerChoice, setAnswerChoice] = useState(false);
     const [answerChoiceCss, setAnswerChoiceCss] = useState(false);
     const [userResult,setUseResult] = useState(0);
+    const [firstGetQuestion, setFirstGetQuestion] = useState(false);
+
     const handleValidation = (values) => {
         setShowAnswer(true);
         setShowButton(true);
@@ -26,7 +30,6 @@ function Training() {
             (typeof values.userChoice === "string"&& values.userChoice in  questionData.correctAnswer)){
             setUseResult(userResult+1);
         }
-
         console.log(userResult);
     };
 
@@ -44,7 +47,8 @@ function Training() {
 
     const handleNextQuestion = (values, {resetForm}) => {
         resetForm();
-        if (index < MOCK_QUESTIONNAIRE.questions.length - 1) {
+        getQuestion();
+        if (index < quizzSize - 1) {
             setIndex(index + 1);
             setShowAnswer(false);
             setShowButton(false);
@@ -58,8 +62,35 @@ function Training() {
         console.log(values);
     };
 
+    useEffect( () => {
+        console.log("useEffect IN !!!");
+        axios.get(`http://localhost:8080/quizz/1/question/0`)
+            .then(res => {
+                if (res.status === 200) {
+                    console.log(res.data);
+                    setQuestionData(res.data);
+                    setFirstGetQuestion(true)
+                }
+            }, (error) => {
+                console.error(error);
+            })
+    }, []);
+
+    const getQuestion = () => {
+        axios.get(`http://localhost:8080/quizz/1/question/${index}`)
+            .then(res => {
+                if (res.status === 200) {
+                    console.log(res.data)
+                    setQuestionData(res.data);
+                }
+            }, (error) => {
+                console.error(error);
+            })
+    }
+
     return (
         <div className="question">
+            {firstGetQuestion && <>
             {!lastQuestion && <div >
                 <h4> {questionData.questionText} </h4>
                 <div className="options-container">
@@ -71,11 +102,12 @@ function Training() {
 
                             {({errors, values, isValid, handleSubmit, handleBlur, handleChange}) => (
                                 <>
+                                    {console.log(questionData)}
                                     {listQuestion &&
                                     <Question question={questionData} show={showAnswer} showButton={showButton}
                                               onHandleValidation={()=>handleValidation(values)} answerChoice={answerChoice}
-                                              answerChoiceCss={answerChoiceCss}  nbQuestion={MOCK_QUESTIONNAIRE.questions.length }
-                                              errors={errors} values={values}
+                                              answerChoiceCss={answerChoiceCss} quizzSize={quizzSize}
+                                              errors={errors} values={values} index={index}
                                               isValid={isValid} handleSubmit={handleSubmit} handleBlur={handleBlur}
                                               handleChange={handleChange}/>}
                                 </>
@@ -89,14 +121,14 @@ function Training() {
             {lastQuestion && <Fragment>
                 <div className=" result">
                     <h2>Exercice termier</h2>
-                    <h3> *** Resultat : {userResult} /{MOCK_QUESTIONNAIRE.questions.length -1} ***</h3>
+                    <h3> *** Resultat : {userResult} /{quizzSize} ***</h3>
                     <h5 className={userResult > 7 ? "correct" : "incorrect"}>
                      {userResult > 7 ? "Bravo ! Vous avez réussi cet exercice !"
                         : "Vous n'avez pas validé ce quiz.Vous n'avez pas atteint le seuil de validation de cet exercice," +
                         " c'est-à-dire 70%. Ce n'est pas très grave car vous pourrez repasser le quiz dans 24h."}</h5>
-
                 </div>
             </Fragment>}
+            </>}
         </div>
     );
 }
--- FrontEnd/src/components/accueil/question.jsx
+++ FrontEnd/src/components/accueil/question.jsx
@@ -7,20 +7,18 @@ import {MyCheckbox} from "./MyCheckbox";
 import classNames from "classnames";
 import Button from "react-bootstrap/Button";
 
-const Question = ({question, show, onHandleValidation, showButton, errors, nbQuestion,answerChoice, values, isValid, handleSubmit, handleBlur, handleChange}) => {
+const Question = ({question, show, onHandleValidation, showButton, errors,index, quizzSize, nbQuestion,answerChoice, values, isValid, handleSubmit, handleBlur, handleChange}) => {
     console.log(question);
     console.log(values);
+
     return (
         <Form onSubmit={handleSubmit} style={{textAlign: "left", marginLeft: "10px"}}>
-
             {question.choices.map((item, idx) => (
                 <div className={classNames("choice-style", {
                     "incorrect": answerChoice && values.userChoice.includes(item.id + '') && !question.correctAnswer.includes(item.id),
                     "correct": answerChoice && question.correctAnswer.includes(item.id)
                 })} key={idx}>
-
                     <div className={"choice-icon"}>
-
                         {answerChoice &&
                         (question.correctAnswer.includes(item.id) || values.userChoice.includes(item.id + '')) &&
                         < span>
@@ -31,15 +29,11 @@ const Question = ({question, show, onHandleValidation, showButton, errors, nbQue
                         }
                         </span>
                         }
-
                     </div>
                     <MyCheckbox className="option" item={item.choice} name={`userChoice`}
                                 value={item.id} type={question.choiceType} idx={item.id} hdlChange={handleChange}
                                 hdlBlur={handleBlur} hdlDisable={answerChoice}/>
-
-
                 </div>
-
             ))}
             {show && <Answer explication={question.explanation}/>}
             {console.log(nbQuestion === question.id)}
@@ -48,13 +42,11 @@ const Question = ({question, show, onHandleValidation, showButton, errors, nbQue
                 <Button variant={"success"} onClick={onHandleValidation}
                         disabled={(!isValid || values['userChoice'].length === 0)}>Valider</Button>}
                 {showButton &&
-                <Button type={'submit'} style={{ background: "linear-gradient(45deg,#ff4b82 0,#ef8f5f 100%)"}}>{!(nbQuestion === question.id)?"Question suivante":"consultez resultat"}</Button>}
-
+                <Button type={'submit'} style={{ background: "linear-gradient(45deg,#ff4b82 0,#ef8f5f 100%)"}}>{!(index+1 === quizzSize)?"Question Suivante":"Consultez Resultat"}</Button>}
             </div>
-            {/*<pre>{JSON.stringify(errors, null, 4)}</pre>
-
-                            <p>--------------------------------------------</p>
-                            <pre>{JSON.stringify(values, null, 4)}</pre>*/}
+            {/* <pre>{JSON.stringify(errors, null, 4)}</pre>
+             <p>--------------------------------------------</p>
+             <pre>{JSON.stringify(values, null, 4)}</pre> */}
         </Form>
     )
 };
