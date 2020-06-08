b4b57c727d95c4362994a2dec8913892fa40a225
Abdelkafi Amal <zaghdanamal@yahoo.fr>
2020-06-08 10:15:58 +0200

    modification component resultqcm pour prendre en compte les erreur

--- FrontEnd/src/components/qcm/resultQcm.jsx
+++ FrontEnd/src/components/qcm/resultQcm.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
 
-const ResultQcm = ({quizzSize,userResult}) => (
-        <div className=" result">
-            <h2>Exercice Termier</h2>
-            <h3> *** Resultat : {userResult} /{quizzSize} ***</h3>
-            <h5 className={userResult > 7 ? "correct" : "incorrect"}>
-                {userResult > 7 ? "Bravo ! Vous avez réussi cet exercice !"
-                    : "Vous n'avez pas validé ce quiz.Vous n'avez pas atteint le seuil de validation de cet exercice," +
-                    " c'est-à-dire 70%. Ce n'est pas très grave car vous pourrez repasser le quiz dans 24h."}</h5>
-        </div>
-            );
+const ResultQcm = ({quizzSize, userResult, resultReview}) => (
+    <div className=" result">
+        <h2>Exercice Termier</h2>
+        <h3> *** Resultat : {userResult} /{quizzSize} ***</h3>
+        <h5 className={userResult > 7 ? "correct" : "incorrect"}>
+            {userResult > 7 ? "Bravo ! Vous avez réussi cet exercice !"
+                : "Vous n'avez pas validé ce quiz.Vous n'avez pas atteint le seuil de validation de cet exercice," +
+                " c'est-à-dire 70%."}</h5>
+        {(resultReview.length !== 0 && userResult < 7  )&& <h4>Compétences évaluées: </h4>}
+        {(resultReview.length !== 0 && userResult < 7  ) && resultReview.map((item, idx) => (
+            <h5 key={idx}> {item}</h5>))}
+        {console.log(resultReview)
+        }
+    </div>
+);
 
 export default ResultQcm;
--- FrontEnd/src/components/training/result.jsx
+++ FrontEnd/src/components/training/result.jsx
@@ -8,7 +8,7 @@ const Result = ({quizzSize,userResult}) => (
             <h5 className={userResult > 7 ? "correct" : "incorrect"}>
                 {userResult > 7 ? "Bravo ! Vous avez réussi cet exercice !"
                     : "Vous n'avez pas validé ce quiz.Vous n'avez pas atteint le seuil de validation de cet exercice," +
-                    " c'est-à-dire 70%. Ce n'est pas très grave car vous pourrez repasser le quiz dans 24h."}</h5>
+                    " c'est-à-dire 70%. "}</h5>
         </div>
             );
 
