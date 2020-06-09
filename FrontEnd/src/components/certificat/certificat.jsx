60942f3911bbf0b96abd852f8a5d1cbca8b57b0c
Stephane PASTORE <stephane.pastore@isil.fr>
2020-06-09 15:09:48 +0200

    Creation de l'affichage d'une question avec du code en mode test

--- FrontEnd/src/components/certificat/certificat.jsx
+++ FrontEnd/src/components/certificat/certificat.jsx
@@ -1,39 +1,79 @@
 
-import React from "react";
-import Form from "react-bootstrap/Form";
+import React, {useEffect} from "react";
+import Prism from "prismjs";
+import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/themes/prism-okaidia.css";
+//import "prismjs/plugins/line-numbers/prism-line-numbers.min"
+import "prismjs/plugins/line-numbers/prism-line-numbers.css"
+import {Form} from "react-bootstrap";
+import Answer from "../shared/question/answer";
+import FormGroup from "react-bootstrap/FormGroup";
+import FormCheck from "react-bootstrap/FormCheck";
+
 
 function Certificat() {
- /*   const [verif, setVerif] = useState(false);
-    const handleResponse = () => {
 
-    }
-    const verifReponse = evt => {
-        evt.preventDefault();
-        console.log('ok');
-        setVerif(true)
-    } */
+    const MOCK_QUESTION_CODE = {
+        id: 9,
+        questionText: `var var = 4;
+text = 'Hello !';
+var variable = 5.781e+8;
+var 1variable = 10;`,
+        type: 'trainning',
+        theme: 'JavaScript',
+        level: 1,
+        choiceType: 'checkbox',
+        choices: [
+            {id: 34, choice: 'var'},
+            {id: 35, choice: 'text'},
+            {id: 36, choice: 'variable'},
+            {id: 37, choice: '1variable'}
+        ],
+        correctAnswer: [36],
+        explanation: 'Pour déclarer une variable, il faut respecter plusieurs étapes :\n' +
+            '\n' +
+            'Écrire le mot-clé var ;\n' +
+            'Utiliser un nom de variable valide, vous pouvez retrouver au début de ce chapitre ce qui constitue un nom de variable valide ;\n' +
+            'Assigner une valeur si on le souhaite, elle peut être un chiffre, un nombre exponentiel, du texte, etc. ;\n' +
+            'Ne pas oublier le point-virgule.'
+    };
 
-    return <div>
+    useEffect(() => {
+        Prism.highlightAll();
+    }, []);
 
-        <Form>
-            <h2> CERTIFICAT </h2>
-            {/*mock_questionnaire.questions.map((question, rang) =>
-                (<>
-                    <h3>{rang + 1}: {question.questionText}</h3>
-                    {question.choices.map((choix, index) => (
-                        <Form.Check type={question.choiceType} id={Date.now().toString()} key={"choix" + index}
-                                    name='test'
-                                    label={choix} onChange={handleResponse}/>
-                    ))}
-                    <button onClick={verifReponse}> Valider</button>
-                    <br/>
-                    {verif && question.explication}
+    return <div className="question">
 
-                </>)
-            )*/}
-        </Form>
+        <div style={{width: "70%", margin: "auto", borderRadius: "5px"}}>
+            <br/>
+            <h5>
+        <pre className={"line-numbers"}>
+        <code className="language-js">
+          {MOCK_QUESTION_CODE.questionText}
+        </code>
+      </pre>
+            </h5>
+            <br/>
+
+            <Form style={{textAlign: "left", marginLeft: "10px"}}>
+                {MOCK_QUESTION_CODE.choices.map((item, idx) => (
+                    <div key={idx}>
 
 
+                        <div key={`custom-${MOCK_QUESTION_CODE.choiceType}-${idx}`}>
+                            <FormGroup>
+                                <FormCheck custom id={`custom-${MOCK_QUESTION_CODE.choiceType}-${idx}`} value={item.id}
+                                           type={MOCK_QUESTION_CODE.choiceType}
+                                           label={item.choice}/>
+                            </FormGroup>
+                        </div>
+
+                    </div>
+                ))}
+                <Answer explication={MOCK_QUESTION_CODE.explanation}/>
+            </Form>
+        </div>
+        ;
     </div>;
 }
 
