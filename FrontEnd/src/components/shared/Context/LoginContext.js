3f280d452d5d0a98f688d33220f40d4ee2ba2d72
Stephane PASTORE <stephane.pastore@isil.fr>
2020-06-24 12:39:10 +0200

    Creation du component LoginContext

---
+++ FrontEnd/src/components/shared/Context/LoginContext.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default React.createContext({
+    loggedInUser: "",
+    isLogged: false,
+    updateLoggedInUser: user => {},
+    updateIsLogged: loggedState => {}
+})
