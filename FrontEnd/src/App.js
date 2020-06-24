4bccb32473c9b5dd950e3830e2f08581e95b9536
Stephane PASTORE <stephane.pastore@isil.fr>
2020-06-24 14:43:47 +0200

    Mise en place du context + provider dans App component

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,21 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Login from "./components/login/Login";
-import { Route } from 'react-router-dom';
-import Accueil from "./components/accueil/Accueil";
-
+import {Route} from 'react-router-dom';
 import {Switch} from "react-router";
+import Login from "./components/login/Login";
+import Accueil from "./components/accueil/Accueil";
+import LoginContext from "./components/shared/Context/LoginContext";
 
 function App() {
 
-  return (
-      <div className="App">
-          <Switch>
-              <Route exact path='/'  component={Login} />
-              <Route path='/Accueil' component={Accueil} />
-          </Switch>
-      </div>
-  );
+    const [loggedInUser, setLoggedInUser] = useState("");
+    const [isLogged, setIsLogged] = useState(false);
+
+    const contextLoginValue = {
+        loggedInUser,
+        isLogged,
+        updateLoggedInUser: setLoggedInUser,
+        updateIsLogged: setIsLogged
+    };
+
+    return (
+        <LoginContext.Provider value={contextLoginValue}>
+            <div className="App">
+                <Switch>
+                    <Route exact path='/' component={Login}/>
+                    <Route path='/Accueil' component={Accueil}/>
+                </Switch>
+            </div>
+        </LoginContext.Provider>
+    );
 }
 
 export default App;
--- FrontEnd/src/components/shared/Context/LoginContext.js
+++ FrontEnd/src/components/shared/Context/LoginContext.js
@@ -5,4 +5,4 @@ export default React.createContext({
     isLogged: false,
     updateLoggedInUser: user => {},
     updateIsLogged: loggedState => {}
-})
+});
