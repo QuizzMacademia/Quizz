43d07ec9c5c6672107b9fbbfe78aaab464df7c39
Stephane PASTORE <stephane.pastore@isil.fr>
2020-06-25 13:21:28 +0200

    Add memo au components Accueil / Footer / Home + Modification Link dans Home

--- FrontEnd/src/components/accueil/footer.jsx
+++ FrontEnd/src/components/accueil/footer.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {memo} from "react";
 import {Link} from "react-router-dom";
 import './footer.css';
 
@@ -43,4 +43,4 @@ function Footer() {
     );
 }
 
-export default Footer;
+export default memo(Footer);
