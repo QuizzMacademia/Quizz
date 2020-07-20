9c8a26bed6716644f49ef651e574d7dcf56c6218
asma gouja <asmagouja@gmail.com>
2020-07-20 09:31:57 +0200

    AG_24hExercisingQuizzExpiration+BugFix

--- BackEnd/quizz.server/src/main/java/quizz/demo/repositories/QuizzRepository.java
+++ BackEnd/quizz.server/src/main/java/quizz/demo/repositories/QuizzRepository.java
@@ -1,8 +1,13 @@
 package quizz.demo.repositories;
 
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import quizz.demo.model.entities.Quizz;
 import quizz.demo.model.entities.QuizzType;
 import quizz.demo.model.entities.Theme;
@@ -12,5 +17,7 @@ public interface QuizzRepository extends JpaRepository<Quizz, Long> {
 	Optional<Quizz> findByTypeAndThemeAndLevel(QuizzType type, Theme theme, int level);
 
 	Optional<Quizz> findByTypeAndThemeThemeAndCategoryCategory(QuizzType type, String theme, String Category);
-
+    
+	@Query("select quizz from Quizz quizz where  quizz.expirationDate <= :currentDate ")
+	List <Quizz> findAllQuizzesExpired(@Param ("currentDate") LocalDateTime currentDate );
 }
--- BackEnd/quizz.server/src/main/java/quizz/demo/repositories/ThemeRepository.java
+++ BackEnd/quizz.server/src/main/java/quizz/demo/repositories/ThemeRepository.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import quizz.demo.model.entities.QuestionType;
 import quizz.demo.model.entities.QuizzType;
 import quizz.demo.model.entities.Theme;
 
@@ -13,6 +14,9 @@ public interface ThemeRepository extends JpaRepository<Theme, Long> {
 
 	Optional<Theme> findThemeByTheme(String theme);
 
-	@Query("select theme from Quizz quizz join quizz.theme theme where quizz.type=?1")
+	@Query("select distinct (theme) from Quizz quizz join quizz.theme theme where quizz.type=?1")
 	Optional<List<Theme>> findQuizzThemeByType(QuizzType type);
+	
+	@Query("select distinct (theme) from Question q join q.theme theme where q.type=?1")
+	Optional<List<Theme>> findQuestionThemeByType(QuestionType type);
 }
