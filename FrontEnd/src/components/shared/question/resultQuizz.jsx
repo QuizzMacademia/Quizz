e45c276daf6d4619cd0343fd8db9eff28929c9af
Abdelkafi Amal <zaghdanamal@yahoo.fr>
2020-07-20 15:53:09 +0200

    modification resultQcm + modif question

--- FrontEnd/src/components/shared/question/resultQuizz.jsx
+++ FrontEnd/src/components/shared/question/resultQuizz.jsx
@@ -8,18 +8,18 @@ const ResultQuizz = ({quizzSize, userResult, resultReview, isQCM}) => (
     <div className=" result">
         <h2>Exercice Termié</h2>
         <h3> *** Resultat : {userResult} /{quizzSize} ***</h3>
-        <h5 className={userResult > 7 ? "correct" : "incorrect"}>
-            {userResult > 7
+        <h5 className={(userResult >(quizzSize * 0.7)) ? "correct" : "incorrect"}>
+            {(userResult > (quizzSize  * 0.7))
                 ? "Bravo ! Vous avez réussi cet exercice !"
                 : "Vous n'avez pas validé ce quiz. Vous n'avez pas atteint le seuil de validation de cet exercice,"
                 + " c'est-à-dire 70%."}
         </h5>
 
-        {isQCM && (resultReview.length !== 0 && userResult < 7)
+        {isQCM && (resultReview.length !== 0 && userResult < (quizzSize * 0.7))
         && <h4 style={{marginLeft: "30px", textIndent: "20px", color: "#5b5a5e"}}>
             Compétences évaluées:
         </h4>}
-        {isQCM && (resultReview.length !== 0 && userResult < 7)
+        {isQCM && (resultReview.length !== 0 && (userResult <  (quizzSize * 0.7)))
         && <Accordion>
             <Card style={{width: "50%", margin: "auto"}}>
                 <Card.Header>
