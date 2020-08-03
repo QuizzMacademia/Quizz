28bb51c85431d336058235d7f4c5d1ddfa77151e
Abdelkafi Amal <zaghdanamal@yahoo.fr>
2020-08-03 15:09:01 +0200

    modification componente certificat

--- FrontEnd/src/components/certificat/certificat.jsx
+++ FrontEnd/src/components/certificat/certificat.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from "react";
-import {Form} from "react-bootstrap";
+import axios from "axios";
 import Button from "react-bootstrap/Button";
 import FormGroup from "react-bootstrap/FormGroup";
 import FormCheck from "react-bootstrap/FormCheck";
@@ -10,10 +10,38 @@ import CodeEditor from "../shared/Code/CodeEditor";
 function Certificat() {
 
     const [codeVal, setCodeVal] = useState("");
-
+    const [showResponse, setShowResponse] = useState(false);
+    const [codeResponse, setCodeResponse] = useState("");
     const majCodeVal = (newval) => {
         setCodeVal(newval);
     };
+
+
+    function checkResponse(){
+        var config = {
+            headers: {
+                'Content-Type': 'text/plain'
+            },
+            responseType: 'text'
+        };
+
+        console.log(codeVal);
+        console.log(typeof codeVal);
+        axios.post('/quizz/codePython', codeVal, config)
+            .then((res)=> {
+                console.log(res.data);
+                if (res.status === 200) {
+                    console.log(codeVal);
+                    setShowResponse(true);
+                    setCodeResponse(res.data.buffer)}
+
+            })
+            .catch((e)=>{
+                console.error(e.response);
+                setShowResponse(true);
+                setCodeResponse(e.response.data.message)
+            });
+    }
 
     function sliceQuestionText(str) {
         const deliminator = "#4#";
@@ -47,17 +75,20 @@ function Certificat() {
 
                             <CodeEditor codeValue={codeVal} uniqueIdName={`code`} codeMode={"python"} majValue={majCodeVal}/>
 
-                        <Button style={{width:"100%", backgroundColor:"#ff4b82", borderRadius:"0  0 7px 7px ", border:"none"}} onClick={() =>{ console.log(codeVal);
-                        console.log(typeof codeVal);}}>Valider</Button>
-                        {console.log()}
-                        {sliceQuestionText(question.explanation).map((item1, idx1) => (
-                            <div key={idx1}>
-                                {item1.type === 'text'
-                                    ? <p>{item1.value}</p>
-                                    : <CodeReadOnly codeValue={item1.value} uniqueIdName={`code-${idx1}`}/>}
-                            </div>
-                        ))}
+                        <Button style={{width:"100%", backgroundColor:"#ff4b82", borderRadius:"0  0 7px 7px ", border:"none"}} onClick={checkResponse}>Valider</Button>
+
+                        {showResponse&& <>
+                            <CodeReadOnly codeValue={codeResponse} uniqueIdName={`code-3`}/>
+                                {sliceQuestionText(question.explanation).map((item1, idx1) => (
+                                <div key={idx1}>
+                                    {item1.type === 'text'
+                                        ? <p>{item1.value}</p>
+                                        : <CodeReadOnly codeValue={item1.value} uniqueIdName={`code-${idx1}`}/>}
+                                </div>
+                            ))}
+                        </>}
                     </div>
+
                 </div>
 
             ))}
